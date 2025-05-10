e3ab31e3e2a0382cd8865bec9a946e8dd09b9c89
Oluwasuyi-Timilehin <oluwasuyitimilehin@gmail.com>
2025-05-10 23:51:09 +0100

    made changes

--- intro/src/Pages/Homepage.jsx
+++ intro/src/Pages/Homepage.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../Components/Navbar";
 
 
-// This is the homepage component that imports and uses the Navbar component
+// This is the Hmomepage component that imports and uses the Navbar component
 const Homepage = () => {
   return (
     <>
